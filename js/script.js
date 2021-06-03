889804018491e01bd65db2dcc7354c2c8adeb085
cromeo98 <romeo.christian98@gmail.com>
2021-06-03 15:37:32 +0200

    stamp (cnsl.log) if mltp 3&5 -> fizzbuzz

--- js/script.js
+++ js/script.js
@@ -8,11 +8,12 @@
 
 // stampo numeri da uno a 100
 for(var i = 1; i < 101; i++){
-    if(i % 3 == 0){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log(i + ' FizzBuzz');
+    } else if(i % 3 == 0){
         console.log(i + ' Fizz');
     } else if(i % 5 == 0){
         console.log(i + ' Buzz');
-    } 
-    console.log(i)
+    }  
 }
 
